8773e1b2797f17473b554c0aa90c8defb679d21b
Tris Pretty <beertastic@gmail.com>
2023-10-12 15:33:54 +0100

    dd testing

--- app/Http/Controllers/FileAdmin/ScanController.php
+++ app/Http/Controllers/FileAdmin/ScanController.php
@@ -76,6 +76,10 @@ class ScanController extends Controller
 
     public function refreshMovies()
     {
+        if ($this->path_movies == '') {
+            dd('Movie path: ' . $this->path_movies);
+        }
+
         $lists = scandir($this->path_movies);
         $lists = array_diff($lists, array(".", "..", "Thumbs.db", "theme.mp3", ".DS_Store", "Desktop_.ini"));
 
